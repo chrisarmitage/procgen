b770bdf854f01244485c62895a72ad747cb490f6
Chris Armitage <dev@chrisarmitage.com>
2018-11-05 15:02:33 +0000

    Add missing ordinaries

--- src/ProcGen/HeraldicOrdinaryGenerator.php
+++ src/ProcGen/HeraldicOrdinaryGenerator.php
@@ -2,6 +2,7 @@
 
 namespace ProcGen;
 
+use SVG\Nodes\Shapes\SVGPath;
 use SVG\Nodes\Shapes\SVGPolygon;
 use SVG\Nodes\SVGNode;
 
@@ -18,7 +19,19 @@ class HeraldicOrdinaryGenerator
         $methods = [
             'bend',
             'cross',
+            'pale',
+            'fess',
+            'saltire',
             'chevron',
+            'chief',
+            'paly',
+            'barry',
+            'chequy',
+            'bendy',
+            'lozengy',
+            'chevronny',
+            'bordure',
+            'orle',
         ];
 
         $method = $methods[mt_rand(0, count($methods) - 1)];
@@ -71,6 +84,339 @@ class HeraldicOrdinaryGenerator
         );
 
         return $polygon;
+    }
+
+    /**
+     * @return SVGNode
+     */
+    public function pale()
+    {
+        $polygon = new SVGPolygon(
+            $this->generatePoints(
+                [
+                    [ -2, -8],
+                    [  2, -8],
+                    [  2,  8],
+                    [ -2,  8],
+                ]
+            )
+        );
+
+        return $polygon;
+    }
+
+    /**
+     * @return SVGNode
+     */
+    public function fess()
+    {
+        $polygon = new SVGPolygon(
+            $this->generatePoints(
+                [
+                    [ -6, -2],
+                    [  6, -2],
+                    [  6,  2],
+                    [ -6,  2],
+                ]
+            )
+        );
+
+        return $polygon;
+    }
+
+    /**
+     * @return SVGNode
+     */
+    public function saltire()
+    {
+        $polygon = new SVGPolygon(
+            $this->generatePoints(
+                [
+                    [ -6, -8],
+                    [  0, -2],
+                    [  6, -8],
+                    [  8, -6],
+                    [  2,  0],
+                    [  8,  6],
+                    [  6,  8],
+                    [  0,  2],
+                    [ -6,  8],
+                    [ -8,  6],
+                    [ -2,  0],
+                    [ -8, -6],
+                    [ -6, -8],
+                ]
+            )
+        );
+
+        return $polygon;
+    }
+
+    /**
+     * @return SVGNode
+     */
+    public function chief()
+    {
+        $polygon = new SVGPolygon(
+            $this->generatePoints(
+                [
+                    [ -6, -6],
+                    [  6, -6],
+                    [  6, -2],
+                    [ -6, -2],
+                ]
+            )
+        );
+
+        return $polygon;
+    }
+
+    /**
+     * @return SVGNode[]
+     */
+    public function paly()
+    {
+        $polygons = [];
+        $polygons[] = new SVGPolygon(
+            $this->generatePoints(
+                [
+                    [ -4, -8],
+                    [ -2, -8],
+                    [ -2,  8],
+                    [ -4,  8],
+                ]
+            )
+        );
+        $polygons[] = new SVGPolygon(
+            $this->generatePoints(
+                [
+                    [  0, -8],
+                    [  2, -8],
+                    [  2,  8],
+                    [  0,  8],
+                ]
+            )
+        );
+        $polygons[] = new SVGPolygon(
+            $this->generatePoints(
+                [
+                    [  4, -8],
+                    [  6, -8],
+                    [  6,  8],
+                    [  4,  8],
+                ]
+            )
+        );
+
+        return $polygons;
+    }
+
+    /**
+     * @return SVGNode[]
+     */
+    public function barry()
+    {
+        $polygons = [];
+        $polygons[] = new SVGPolygon(
+            $this->generatePoints(
+                [
+                    [ -6, -6],
+                    [  6, -6],
+                    [  6, -4],
+                    [ -6, -4],
+                ]
+            )
+        );
+        $polygons[] = new SVGPolygon(
+            $this->generatePoints(
+                [
+                    [ -6, -2],
+                    [  6, -2],
+                    [  6,  0],
+                    [ -6,  0],
+                ]
+            )
+        );
+        $polygons[] = new SVGPolygon(
+            $this->generatePoints(
+                [
+                    [ -6,  2],
+                    [  6,  2],
+                    [  6,  4],
+                    [ -6,  4],
+                ]
+            )
+        );
+        $polygons[] = new SVGPolygon(
+            $this->generatePoints(
+                [
+                    [ -6,  6],
+                    [  6,  6],
+                    [  6,  8],
+                    [ -6,  8],
+                ]
+            )
+        );
+
+        return $polygons;
+    }
+
+
+    /**
+     * @return SVGNode[]
+     */
+    public function chequy()
+    {
+        $polygons = [];
+
+        for ($y = -6; $y <= 8; $y += 4) {
+
+            for ($x = -6; $x <= 6; $x += 4) {
+
+                $polygons[] = new SVGPolygon(
+                    $this->generatePoints(
+                        [
+                            [ $x    , $y],
+                            [ $x + 2, $y],
+                            [ $x + 2, $y + 2],
+                            [ $x    , $y + 2],
+                        ]
+                    )
+                );
+                $polygons[] = new SVGPolygon(
+                    $this->generatePoints(
+                        [
+                            [ $x + 2, $y + 2],
+                            [ $x + 4, $y + 2],
+                            [ $x + 4, $y + 4],
+                            [ $x + 2, $y + 4],
+                        ]
+                    )
+                );
+            }
+        }
+
+        return $polygons;
+    }
+
+
+    /**
+     * @return SVGNode[]
+     */
+    public function bendy()
+    {
+        $polygons = [];
+
+        for ($y = -14; $y <= 12; $y += 8) {
+            $polygons[] = new SVGPolygon(
+                $this->generatePoints(
+                    [
+                        [ -6, $y],
+                        [  8, $y + 14],
+                        [  6, $y + 16],
+                        [ -8, $y + 2],
+                    ]
+                )
+            );
+        }
+
+        return $polygons;
+    }
+
+    /**
+     * @return SVGNode[]
+     */
+    public function lozengy()
+    {
+        $polygons = [];
+
+        for ($y = -9; $y <= 12; $y += 3) {
+            for ($x = -6; $x <= 6; $x += 3) {
+                $polygons[] = new SVGPolygon(
+                    $this->generatePoints(
+                        [
+                            [ $x, $y],
+                            [ $x + 1.5, $y + 1.5],
+                            [ $x, $y + 3],
+                            [ $x - 1.5, $y + 1.5],
+                        ]
+                    )
+                );
+            }
+        }
+
+        return $polygons;
+    }
+
+    /**
+     * @return SVGNode[]
+     */
+    public function chevronny()
+    {
+        $polygons = [];
+
+        for ($y = -14; $y <= 10; $y += 8) {
+            $polygons[] = new SVGPolygon(
+                $this->generatePoints(
+                    [
+                        [  0, $y],
+                        [  8, $y + 8],
+                        [  6, $y + 10],
+                        [  0, $y + 4],
+                        [ -6, $y + 10],
+                        [ -8, $y + 8],
+                    ]
+                )
+            );
+        }
+
+        return $polygons;
+    }
+
+    /**
+     * @return
+     */
+    public function bordure()
+    {
+
+        $unit = $this->unitSize * 0.75;
+        $center = $this->size / 2;
+
+        $path = sprintf('M %d %d', $center + ($unit * -5), $center +  ($unit * -6))
+            . sprintf( ' L %d %d', $center + ($unit * 6), $center +  ($unit * -6))
+            . sprintf( ' L %d %d', $center + ($unit * 6), $center +  ($unit * -2))
+            . sprintf( ' A %d %d 0 0 1 %d %d', $unit * 16, $unit * 16, $center + ($unit * 0), $center +  ($unit * 8))
+            . sprintf( ' A %d %d 0 0 1 %d %d', $unit * 16, $unit * 16, $center + ($unit * -6), $center +  ($unit * -2))
+            . sprintf( ' L %d %d', $center + ($unit * -6), $center +  ($unit * -6))
+            . sprintf( ' L %d %d', $center + ($unit * -5), $center +  ($unit * -6))
+        ;
+        $outline = new SVGPath($path);
+
+        return $outline;
+    }
+
+    /**
+     * @return
+     */
+    public function orle()
+    {
+
+        $unit = $this->unitSize * 0.75;
+        $center = $this->size / 2;
+
+        $path = sprintf('M %d %d', $center + ($unit * -5), $center +  ($unit * -6))
+            . sprintf( ' L %d %d', $center + ($unit * 6), $center +  ($unit * -6))
+            . sprintf( ' L %d %d', $center + ($unit * 6), $center +  ($unit * -2))
+            . sprintf( ' A %d %d 0 0 1 %d %d', $unit * 16, $unit * 16, $center + ($unit * 0), $center +  ($unit * 8))
+            . sprintf( ' A %d %d 0 0 1 %d %d', $unit * 16, $unit * 16, $center + ($unit * -6), $center +  ($unit * -2))
+            . sprintf( ' L %d %d', $center + ($unit * -6), $center +  ($unit * -6))
+            . sprintf( ' L %d %d', $center + ($unit * -5), $center +  ($unit * -6))
+        ;
+        $outline = new SVGPath($path);
+        $outline->setStyle('fill', 'none')
+            ->setStyle('stroke-width', ($this->unitSize * 0.75) . 'px');
+
+        return $outline;
     }
 
     /**
