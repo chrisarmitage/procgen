2e87fac31f79e1b61ef82058edd9bb1335864542
Chris Armitage <dev@chrisarmitage.com>
2018-11-08 14:55:54 +0000

    Working version of offset lines / miter joins

--- public/dim.php
+++ public/dim.php
@@ -1,23 +1,44 @@
 <?php
 
+ini_set('display_errors', 'on');
 
 use SVG\SVG;
 
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/vector-lib.php';
 
-$image = new SVG(200, 200);
+$image = new SVG(500, 800);
 $doc = $image->getDocument();
 
+//$points = [
+//    [ 20, 100],
+//    [180, 100],
+//];
+//$points = [
+//    [ 20, 20],
+//    [180, 180],
+//];
 $points = [
-    [ 20, 100],
-    [180, 100],
+    [ 40, 40],
+    [ 160, 40],
+    [ 160, 100],
+    [ 260, 100],
+    [ 260, 160],
+    [ 200, 220],
+    [ 100, 220],
+    [  50, 210],
+    [ 100, 160],
+    [  10, 160],
 ];
-$points = [
-    [ 20, 20],
-    [180, 180],
-];
+//$points = [
+//    [ 40, 40],
+//    [ 160, 40],
+//    [ 260, 140],
+//];
 
-$nodes = makeTwinPair($points);
+$textLines = [];
+
+$nodes = makeMiterPair($points, $textLines);
 
 foreach ($nodes as $node) {
     $doc->addChild($node);
@@ -49,6 +70,123 @@ function makePair($points) {
     $line = makeLine(shiftLine($points, 90, 20));
     $line->setStyle('stroke', '#000000')
         ->setStyle('stroke-width', '20');
+    $lines[] = $line;
+
+    return $lines;
+}
+
+function makeMiterPair($points, &$textLines) {
+    $lines = [];
+    $rightPoints = [];
+    $leftPoints = [];
+
+    $initRightPoint = shiftLine($points, +90, 20);
+    $rightPoints[] = $initRightPoint[0];
+    $initLeftPoint = shiftLine($points, -90, 20);
+    $leftPoints[] = $initLeftPoint[0];
+
+    while (count($points) >= 3) {
+
+        $deg = rad2deg(
+            atan2(
+                $points[1][0] - $points[0][0],
+                $points[1][1] - $points[0][1]
+            )
+        );
+        $textLines[] = "** Deg {$deg} for "
+            . $points[1][0] . ' '
+            . $points[0][0] . ' / '
+            . $points[1][1] . ' '
+            . $points[0][1] . ' ('
+            . ($points[1][0] - $points[0][0]) . ' / ' . ($points[1][1] - $points[0][1]) . ')';
+
+        $v1 = new \webd\vectors\Vector(
+            $points[1][0] - $points[0][0],
+            $points[1][1] - $points[0][1]
+        );
+        $v2 = new \webd\vectors\Vector(
+            $points[2][0] - $points[1][0],
+            $points[2][1] - $points[1][1]);
+
+        $tangent = $v1->normalize()->add($v2->normalize())->normalize();
+        $textLines[] = 'Tangent: ' . $tangent->getValue()[0] . ' / ' . $tangent->getValue()[1];
+
+        $mitre = new \webd\vectors\Vector(- $tangent->getValue()[1], $tangent->getValue()[0]);
+
+        $textLines[] = "Mitre Vector: " . $mitre->getValue()[0] . ' / ' . $mitre->getValue()[1];
+
+        $normal = (new \webd\vectors\Vector( - $v1->getValue()[1], $v1->getValue()[0]))->normalize();
+
+        $dLength = 20 / $mitre->dotProduct($normal);
+        $textLines[] = "d = {$dLength}";
+        $textLines[] = "normal = {$normal->getValue()[0]} / {$normal->getValue()[1]}";
+
+        $pin = new \SVG\Nodes\Shapes\SVGCircle(
+            $points[1][0] + ($dLength * $mitre->getValue()[0]),
+            $points[1][1] + ($dLength * $mitre->getValue()[1]),
+            4
+        );
+        $pin->setStyle('fill', '#00cc00');
+        $lines[] = $pin;
+
+        $rightPoints[] = [
+            $points[1][0] + ($dLength * $mitre->getValue()[0]),
+            $points[1][1] + ($dLength * $mitre->getValue()[1]),
+        ];
+
+
+        $pin = new \SVG\Nodes\Shapes\SVGCircle(
+            $points[1][0] - ($dLength * $mitre->getValue()[0]),
+            $points[1][1] - ($dLength * $mitre->getValue()[1]),
+            4
+        );
+        $pin->setStyle('fill', '#0000cc');
+        $lines[] = $pin;
+
+        $leftPoints[] = [
+            $points[1][0] - ($dLength * $mitre->getValue()[0]),
+            $points[1][1] - ($dLength * $mitre->getValue()[1]),
+        ];
+
+        $line = makeLine(shiftLine($points, -90, 20));
+        $line->setStyle('stroke', '#000000')
+            ->setStyle('stroke-width', '1');
+        //$lines[] = $line;
+
+        $line = makeLine(shiftLine($points, 90, 20));
+        $line->setStyle('stroke', '#000000')
+            ->setStyle('stroke-width', '1');
+        //$lines[] = $line;
+
+        array_shift($points);
+    }
+
+    $line = makeLine(shiftLine($points, -90, 20));
+    $line->setStyle('stroke', '#000000')
+        ->setStyle('stroke-width', '1');
+    //$lines[] = $line;
+
+    $line = makeLine(shiftLine($points, 90, 20));
+    $line->setStyle('stroke', '#000000')
+        ->setStyle('stroke-width', '1');
+    //$lines[] = $line;
+
+
+    $finalRightPoint = shiftLine($points, +90, 20);
+    $rightPoints[] = $finalRightPoint[1];
+    $finalLeftPoint = shiftLine($points, -90, 20);
+    $leftPoints[] = $finalLeftPoint[1];
+
+    $line = makeLine($rightPoints);
+    $line->setStyle('stroke', '#000000')
+        ->setStyle('stroke-width', '1')
+        ->setStyle('fill', 'none');
+    $lines[] = $line;
+
+    $line = makeLine($leftPoints);
+    $line->setStyle('stroke', '#000000')
+        ->setStyle('stroke-width', '1')
+        ->setStyle('fill', 'none');
     $lines[] = $line;
 
     return $lines;
@@ -105,8 +243,16 @@ function shiftLine($points, $angle, $distance) {
 }
 
 $marker = makeLine($points);
-$marker->setStyle('stroke', '#CC0000');
+$marker->setStyle('stroke', '#CC0000')
+    ->setStyle('fill', 'none');
 $doc->addChild($marker);
+
+$textY = 300;
+foreach ($textLines as $textLine) {
+    $text = new \SVG\Nodes\Texts\SVGText($textLine, 10, $textY);
+    $doc->addChild($text);
+    $textY += 18;
+}
 
 header('Content-Type: image/svg+xml');
 echo $image;
---
+++ public/vector-lib.php
@@ -0,0 +1,281 @@
+<?php
+
+/**
+ * https://github.com/tdebatty/php-vector-matrix
+ */
+
+namespace webd\vectors;
+
+class Vector
+{
+    protected $value = array();
+
+    public function __construct($value = null) {
+        if (is_null($value)) {
+            return;
+        }
+
+        if (is_array($value)) {
+            $this->value = $value;
+            return;
+        }
+
+        $this->value = func_get_args();
+    }
+
+    /**
+     *
+     * @return [float] Array of value
+     */
+    public function getValue() {
+        return $this->value;
+    }
+
+    public function display($delimiter = "\t") {
+        echo implode($delimiter, $this->value) . "\n";
+    }
+
+    // ----------------- Standard operations
+
+    /**
+     * Sum of 2 Vectors OR
+     * Sum of a Vector + a numeric
+     *
+     * @param Vector or numeric $other
+     * @return \webd\vectors\Vector (or child of...)
+     */
+    public function add($other) {
+        $class = get_called_class();
+        $array = array();
+
+        if (is_numeric($other)) {
+            for ($i=0; $i<count($this->value); $i++) {
+                $array[$i] = $this->value[$i] + $other;
+            }
+
+        } else {
+            for ($i=0; $i<count($this->value); $i++) {
+                $array[$i] = $this->value[$i] + $other->value[$i];
+            }
+        }
+
+        return new $class($array);
+    }
+
+    public function sub($other) {
+        $class = get_called_class();
+        $array = array();
+
+        if (is_numeric($other)) {
+            for ($i=0; $i<count($this->value); $i++) {
+                $array[$i] = $this->value[$i] - $other;
+            }
+
+        } else {
+            for ($i=0; $i<count($this->value); $i++) {
+                $array[$i] = $this->value[$i] - $other->value[$i];
+            }
+        }
+
+        return new $class($array);
+    }
+
+    public function dotProduct(Vector $other) {
+        $accumulator = 0;
+        for ($i=0; $i<count($this->value); $i++) {
+            $accumulator += ($this->value[$i] * $other->value[$i]);
+        }
+        return $accumulator;
+    }
+
+    public function crossProduct (Vector $other) {
+        if ($this->length() != 3) {
+            throw new Exception("Must be 3 dimension");
+        }
+
+        if ($other->length() != 3) {
+            throw new Exception("Must be 3 dimension");
+        }
+
+
+        $class = get_called_class();
+        $result = new $class;
+        /* @var $result Vector */
+        $result->value = array(
+            $this->value[1] * $other->value[2] - $this->value[2] * $other->value[1],
+            $this->value[2] * $other->value[0] - $this->value[0] * $other->value[2],
+            $this->value[0] * $other->value[1] - $this->value[1] * $other->value[0]
+        );
+
+        return $result;
+    }
+
+    public function div($other) {
+        $class = get_called_class();
+        $array = array();
+
+        for ($i=0; $i<count($this->value); $i++) {
+            $array[$i] = $this->value[$i] / $other;
+        }
+
+        return new $class($array);
+    }
+
+
+
+    // ----------------- If PECL extension OPERATOR is installed...
+    public function __add($other) {
+        return $this->add($other);
+    }
+
+    public function __sub($other) {
+        return $this->sub($other);
+    }
+
+    public function __concat(Vector $other) {
+        return $this->dotProduct($other);
+    }
+
+    public function __mul($other) {
+        return $this->crossProduct($other);
+    }
+
+    public function __div($other) {
+        return $this->div($other);
+    }
+
+    // ----------------- Some statistical operations
+    /**
+     * Compute the sample mean (estimated mean) of the values
+     * @return float
+     */
+    public function mean() {
+        $count = count($this->value);
+        $sum = array_sum($this->value);
+        return ((float) $sum) / $count;
+    }
+
+    /**
+     * Computes the UNBIASED sample variance (estimated variance) of the values
+     * TODO: implement a more efficient algorithm:
+     * http://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
+     * @return float
+     */
+    public function variance() {
+        $n = count($this->value);
+        $accumulator = 0.0;
+        for ($i=0; $i < $n; $i++) {
+            $accumulator += $this->value[$i]* $this->value[$i];
+        }
+
+        return $accumulator / ($n-1) - ($n/($n-1)) * $this->mean() * $this->mean();
+    }
+
+    /**
+     * Compute the sample standard deviation (estimated standard deviation) of the values
+     * @return float
+     */
+    public function standardDeviation() {
+        return sqrt($this->variance());
+    }
+
+    /**
+     *
+     * @return Vector with mean = 0 and stdandard deviation = 1
+     */
+    public function standardNormal() {
+        return $this->sub($this->mean())->div($this->standardDeviation());
+    }
+
+
+    public function sort() {
+        $class = get_called_class();
+        $value = $this->value; // Makes a copy of $this->value
+        sort($value);
+        return new $class($value);
+    }
+
+    /**
+     * Compute the Empirical Cumulative Distribution Function for $value
+     * Also called the Kaplan-Meier estimate
+     * ecdf($value) = (number of values <= $value) / number of values
+     * @param float $value
+     * @return float
+     */
+    public function ecdf($value) {
+        $n = $this->length();
+        $count = 0;
+        for ($i=0; $i<$n; $i++) {
+            if ($this->value[$i] <= $value) {
+                $count++;
+            }
+        }
+        return $count / $n;
+    }
+
+    /**
+     * Tests if the values of the vector follow a normal law, using Anderson - Darling test
+     * http://www.itl.nist.gov/div898/handbook/eda/section3/eda35e.htm
+     * http://en.wikipedia.org/wiki/Anderson%E2%80%93Darling_test
+     * With significance level (alpha) = 1%
+     * Procedure is valid for sample size at least 8 values
+     *
+     * @return boolean true is values follow a normal law
+     */
+    public function adtest() {
+        $critical = 1.092; // Corresponds to alpha = 0.01
+        $nd = new \webd\stats\NormalDistribution();
+        $sorted = $this->sort();
+        $n = $this->length();
+        $A2 = -$n;
+        for ($i = 1; $i <= $n; $i++) {
+            $A2 += -(2 * $i - 1) / $n * ( log($nd->cumulativeProbability($sorted->value[$i - 1])) + log(1 - $nd->cumulativeProbability($sorted->value[$n - $i])) );
+        }
+        $A2_star = $A2 * (1 + 4 / $n - 25 / ($n * $n));
+        if ($A2_star > $critical) {
+            return FALSE;
+        } else {
+            // Data seems to follow a normal law
+            return TRUE;
+        }
+    }
+
+    public function isGaussian() {
+        return $this->adtest();
+    }
+
+    /**
+     * Returns normalized vector (vector with same direction but length = 1)
+     * @return Vector
+     */
+    public function normalize() {
+        return $this->div($this->norm());
+    }
+
+    /**
+     *
+     * @return float Eucledian norm of the vector
+     * @todo Implement p-norm: http://en.wikipedia.org/wiki/Norm_(mathematics)
+     */
+    public function norm() {
+        $accumulator = 0;
+        for ($i=0; $i<count($this->value); $i++) {
+            $accumulator += ($this->value[$i] * $this->value[$i]);
+        }
+        return sqrt($accumulator);
+    }
+
+    public function length() {
+        return count($this->value);
+    }
+
+    /**
+     * Compute the scalar projection of vector $a on this vector
+     * aka scalar resolute or scalar component of a in the direction of this vector
+     * http://en.wikipedia.org/wiki/Scalar_projection
+     * @param Vector $a
+     */
+    public function scalarProject(Vector $a) {
+        return $this->dotProduct($a) / $this->norm();
+    }
+}
