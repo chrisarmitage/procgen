cd59891fc3f031e82cfcf83bd40655dbd0b309e9
Chris Armitage <dev@chrisarmitage.com>
2018-11-20 14:53:15 +0000

    Investigating line variations

--- public/line-variation.php
+++ public/line-variation.php
@@ -5,6 +5,7 @@ use SVG\Nodes\Structures\SVGDefs;
 use SVG\Nodes\Structures\SVGDocumentFragment;
 use SVG\Nodes\SVGGenericNodeType;
 use SVG\SVG;
+use webd\vectors\Vector;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/vector-lib.php';
@@ -47,7 +48,7 @@ $originalLine->setStyle('stroke', '#00ff00')
     ->setStyle('fill', 'none');
 $doc->addChild($originalLine);
 
-$originalVector = new \webd\vectors\Vector(
+$originalVector = new Vector(
     $points[1][0] - $points[0][0],
     $points[1][1] - $points[0][1]
 );
@@ -66,26 +67,186 @@ for ($n = 1; $n <= 10; $n++) {
     ];
 }
 
+/**
+ * Engrailed
+ */
 $path = vsprintf('M %d %d ', $combinedPoints[0]);
 foreach (array_slice($combinedPoints, 1) as $newPoint) {
     $path .= vsprintf('A 1,1 0 0 0 %d,%d ', $newPoint);
 }
 
+$combinedLine = new \SVG\Nodes\Shapes\SVGPath($path);
+$combinedLine->setStyle('stroke', '#ff0000')
+    ->setStyle('fill', 'none');
+$combinedLine->setAttribute('marker-start', 'url(#marker-circle)')
+    ->setAttribute('marker-mid', 'url(#marker-circle)')
+    ->setAttribute('marker-end', 'url(#marker-circle)');
+$doc->addChild($combinedLine);
 
 
+/**
+ * Zig1
+ */
+$startingPoints = [
+    [ 10, 120]
+];
+
+$path = vsprintf('M %d %d ', $startingPoints[0]);
+for ($n = 1; $n <= 10; $n++) {
+    $path .= ' l 10 -10'
+        . ' l 20 20'
+        . ' l 10 -10';
+}
 
 $combinedLine = new \SVG\Nodes\Shapes\SVGPath($path);
 $combinedLine->setStyle('stroke', '#ff0000')
     ->setStyle('fill', 'none');
-//$combinedLine->setAttribute('marker-start', 'url(#marker-circle)')
-//    ->setAttribute('marker-mid', 'url(#marker-circle)')
-//    ->setAttribute('marker-end', 'url(#marker-circle)');
+$combinedLine->setAttribute('marker-start', 'url(#marker-circle)')
+    ->setAttribute('marker-mid', 'url(#marker-circle)')
+    ->setAttribute('marker-end', 'url(#marker-circle)');
 $doc->addChild($combinedLine);
 
 
 
+/**
+ * Zig2
+ */
+$startingPoints = [
+    [ 10, 180]
+];
+
+$stepPoints = [
+    [ 0, 0],
+    [ 10, 0],
+];
+
+
+for ($yOffset = 0; $yOffset >= 0; $yOffset -= 0.25) {
+    $s1 = generateVectorOffsetPoints(
+        new Point(0, 0),
+        new Point(1, $yOffset),
+        10,
+        10
+    );
+    $s2 = generateVectorOffsetPoints(
+        new Point(0, 0),
+        new Point(1, $yOffset),
+        20,
+        -20
+    );
+
+    $path = vsprintf('M %d %d ', $startingPoints[0]);
+    for ($n = 1; $n <= 10; $n++) {
+        $path .= vsprintf(
+            ' l %d %d l %d %d l %d %d',
+            [
+                $s1->x, -$s1->y,
+                $s2->x, -$s2->y,
+                $s1->x, -$s1->y,
+            ]
+        );
+    }
+
+    $combinedLine = new \SVG\Nodes\Shapes\SVGPath($path);
+    $combinedLine->setStyle('stroke', '#ff00ff')
+        ->setStyle('fill', 'none');
+    $combinedLine->setAttribute('marker-start', 'url(#marker-circle)')
+        ->setAttribute('marker-mid', 'url(#marker-circle)')
+        ->setAttribute('marker-end', 'url(#marker-circle)');
+    $doc->addChild($combinedLine);
+}
+
+
+/**
+ * Embattled
+ */
+for ($yOffset = 0; $yOffset >= -1; $yOffset -= 0.25) {
+    $pLeft = generateVectorOffsetPoints(
+        new Point(0, 0),
+        new Point(1, $yOffset),
+        0,
+        20
+    );
+    $pForwards = generateVectorOffsetPoints(
+        new Point(0, 0),
+        new Point(1, $yOffset),
+        20,
+        0
+    );
+    $pRight = generateVectorOffsetPoints(
+        new Point(0, 0),
+        new Point(1, $yOffset),
+        0,
+        -20
+    );
+
+    $path = sprintf('M %d %d ', $startingPoints[0][0], $startingPoints[0][1] + 300);
+    for ($n = 1; $n <= 10; $n++) {
+        $path .= vsprintf(
+            ' l %d %d l %d %d l %d %d l %d %d',
+            [
+                $pForwards->x, $pForwards->y,
+                $pLeft->x, $pLeft->y,
+                $pForwards->x, $pForwards->y,
+                $pRight->x, $pRight->y,
+            ]
+        );
+    }
+
+    $combinedLine = new \SVG\Nodes\Shapes\SVGPath($path);
+    $combinedLine->setStyle('stroke', '#ff00ff')
+        ->setStyle('fill', 'none');
+//    $combinedLine->setAttribute('marker-start', 'url(#marker-circle)')
+//        ->setAttribute('marker-mid', 'url(#marker-circle)')
+//        ->setAttribute('marker-end', 'url(#marker-circle)');
+    $doc->addChild($combinedLine);
+}
+
+
+
+//die();
+
+
 header('Content-Type: image/svg+xml');
 echo $image;
+
+function generateVectorOffsetPoints(Point $start, Point $end, $vectorDistance, $offsetDistance) {
+    $vectorAngle = rad2deg(
+        atan2(
+            $end->y - $start->y,
+            $end->x - $start->x
+        )
+    );
+
+    $offsetAngle = deg2rad($vectorAngle + 90);
+
+    $newPoint = new Point(
+        $start->x + ($offsetDistance * (cos($offsetAngle))) + ($vectorDistance * (cos(deg2rad($vectorAngle)))),
+        $start->y + ($offsetDistance * (sin($offsetAngle))) + ($vectorDistance * (sin(deg2rad($vectorAngle))))
+    );
+    //var_dump($newPoint);
+
+    return $newPoint;
+}
+
+class Point
+{
+    public $x;
+    public $y;
+
+    /**
+     * Point constructor.
+     * @param $x
+     * @param $y
+     */
+    public function __construct($x, $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
+}
+
+
 
 function drawGrid(SVGDocumentFragment $doc, $size)
 {
@@ -112,4 +273,28 @@ function drawGrid(SVGDocumentFragment $doc, $size)
         $line->setStyle('stroke', '#999999');
         $doc->addChild($line);
     }
+}
+
+function generateOffsetPoints($points, $angle, $distance) {
+    $vectorAngle = rad2deg(
+        atan2(
+            $points[1][1] - $points[0][1],
+            $points[1][0] - $points[0][0]
+        )
+    );
+
+    $offsetAngle = deg2rad($vectorAngle + $angle);
+
+    $newPoints = [
+        [
+            $points[0][0] + ($distance * (cos($offsetAngle))),
+            $points[0][1] + ($distance * (sin($offsetAngle))),
+        ],
+        [
+            $points[1][0] + ($distance * (cos($offsetAngle))),
+            $points[1][1] + ($distance * (sin($offsetAngle))),
+        ],
+    ];
+
+    return $newPoints;
 }
