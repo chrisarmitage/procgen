994adc8a5bb30eda42a27631e0c360d5d827ed31
Chris Armitage <dev@chrisarmitage.com>
2018-11-20 14:54:04 +0000

    First attempt at line variation (invected, engrailed, embattled) on Fess and Band

--- src/ProcGen/HeraldicPartyGenerator.php
+++ src/ProcGen/HeraldicPartyGenerator.php
@@ -2,7 +2,9 @@
 
 namespace ProcGen;
 
+use SVG\Nodes\Shapes\SVGPath;
 use SVG\Nodes\Shapes\SVGPolygon;
+use webd\vectors\Vector;
 
 class HeraldicPartyGenerator
 {
@@ -47,15 +49,192 @@ class HeraldicPartyGenerator
 
     protected function bend()
     {
-        $polygon = new SVGPolygon(
-            $this->generatePoints(
-                [
-                    [ -6, -6],
-                    [  6, -6],
-                    [  6,  6],
-                ]
-            )
-        );
+
+        $variation = $this->params['lineVariation'] ?? 'none';
+
+        switch ($variation) {
+            case 'invected':
+                $points = $this->generatePoints(
+                    [
+                        [ -6, -6],
+                        [  6,  6],
+                        [  6,  8],
+                        [ -6,  8],
+                    ]
+                );
+                $originalVector = new Vector(
+                    $points[1][0] - $points[0][0],
+                    $points[1][1] - $points[0][1]
+                );
+                $splitVectors = $originalVector->div(5);
+
+                $combinedPoints = [
+                    $points[0],
+                ];
+
+                for ($n = 1; $n <= 10; $n++) {
+                    $combinedPoints[] = [
+                        $combinedPoints[$n-1][0] + $splitVectors->getValue()[0],
+                        $combinedPoints[$n-1][1] + $splitVectors->getValue()[1],
+                    ];
+                }
+
+                $path = vsprintf('M %d %d ', $points[0]);
+                $variatedLine = array_shift($points);
+
+                foreach (array_slice($combinedPoints, 1) as $newPoint) {
+                    $path .= vsprintf('A 1,1 0 0 1 %d,%d ', $newPoint);
+                }
+
+                foreach (array_slice($points, 1) as $linePoints) {
+                    $path .= vsprintf('L %d %d ', $linePoints);
+                }
+
+                $polygon = new SVGPath($path);
+                break;
+            case 'engrailed':
+                $points = $this->generatePoints(
+                    [
+                        [ -6, -6],
+                        [  6,  6],
+                        [  6,  8],
+                        [ -6,  8],
+                    ]
+                );
+                $originalVector = new Vector(
+                    $points[1][0] - $points[0][0],
+                    $points[1][1] - $points[0][1]
+                );
+                $splitVectors = $originalVector->div(5);
+
+                $combinedPoints = [
+                    $points[0],
+                ];
+
+                for ($n = 1; $n <= 10; $n++) {
+                    $combinedPoints[] = [
+                        $combinedPoints[$n-1][0] + $splitVectors->getValue()[0],
+                        $combinedPoints[$n-1][1] + $splitVectors->getValue()[1],
+                    ];
+                }
+
+                $path = vsprintf('M %d %d ', $points[0]);
+                $variatedLine = array_shift($points);
+
+                foreach (array_slice($combinedPoints, 1) as $newPoint) {
+                    $path .= vsprintf('A 1,1 0 0 0 %d,%d ', $newPoint);
+                }
+
+                foreach (array_slice($points, 1) as $linePoints) {
+                    $path .= vsprintf('L %d %d ', $linePoints);
+                }
+
+                $polygon = new SVGPath($path);
+                break;
+            case 'embattled':
+                $points = $this->generatePoints(
+                    [
+                        [ -6, -6],
+                        [  6,  6],
+                        [  6,  8],
+                        [ -6,  8],
+                    ]
+                );
+                $originalVector = new Vector(
+                    $points[1][0] - $points[0][0],
+                    $points[1][1] - $points[0][1]
+                );
+                $splitVectors = $originalVector->div(4);
+
+                $combinedPoints = [
+                    $points[0],
+                ];
+
+                for ($n = 1; $n <= 4; $n++) {
+                    $combinedPoints[] = [
+                        $combinedPoints[$n-1][0] + $splitVectors->getValue()[0],
+                        $combinedPoints[$n-1][1] + $splitVectors->getValue()[1],
+                    ];
+                }
+
+                $path = vsprintf('M %d %d ', $points[0]);
+
+
+                /**
+                 * Embattled
+                 */
+
+                $pLeft = $this->generateVectorOffsetPoints(
+                    new Point(0, 0),
+                    new Point(1, 1),
+                    0,
+                    1.5 * $this->unitSize
+                );
+                $pForwards = $this->generateVectorOffsetPoints(
+                    new Point(0, 0),
+                    new Point(1, 1),
+                    1.5 * $this->unitSize,
+                    0
+                );
+                $pRight = $this->generateVectorOffsetPoints(
+                    new Point(0, 0),
+                    new Point(1, 1),
+                    0,
+                    -1.5 * $this->unitSize
+                );
+
+                for ($n = 1; $n <= 4; $n++) {
+                    $path .= vsprintf(
+                        ' l %d %d l %d %d l %d %d l %d %d',
+                        [
+                            $pForwards->x, $pForwards->y,
+                            $pLeft->x, $pLeft->y,
+                            $pForwards->x, $pForwards->y,
+                            $pRight->x, $pRight->y,
+                        ]
+                    );
+                }
+
+                //$variatedLine = array_shift($points);
+
+
+                foreach (array_slice($points, 1) as $linePoints) {
+                    $path .= vsprintf('L %d %d ', $linePoints);
+                }
+
+                $polygon = new SVGPath($path);
+                break;
+            default:
+                $points = $this->generatePoints(
+                    [
+                        [ -6, -6],
+                        [  6,  6],
+                        [  6,  8],
+                        [ -6,  8],
+                    ]
+                );
+                //$variatedLine = array_shift($points);
+
+                $path = vsprintf('M %d %d ', $points[0]);
+                foreach (array_slice($points, 1) as $linePoints) {
+                    $path .= vsprintf('L %d %d ', $linePoints);
+                }
+
+                $polygon = new SVGPath($path);
+                break;
+        }
+
+
+        //        $polygon = new SVGPolygon(
+        //            $this->generatePoints(
+        //                [
+        //                    [ -6, -6],
+        //                    [  6, -6],
+        //                    [  6,  0],
+        //                    [ -6,  0],
+        //                ]
+        //            )
+        //        );
 
         return $polygon;
     }
@@ -98,16 +277,192 @@ class HeraldicPartyGenerator
 
     protected function fess()
     {
-        $polygon = new SVGPolygon(
-            $this->generatePoints(
-                [
-                    [ -6, -6],
-                    [  6, -6],
-                    [  6,  0],
-                    [ -6,  0],
-                ]
-            )
-        );
+
+        $variation = $this->params['lineVariation'] ?? 'none';
+
+        switch ($variation) {
+            case 'invected':
+                $points = $this->generatePoints(
+                    [
+                        [ -6,  0],
+                        [  6,  0],
+                        [  6,  8],
+                        [ -6,  8],
+                    ]
+                );
+                $originalVector = new Vector(
+                    $points[1][0] - $points[0][0],
+                    $points[1][1] - $points[0][1]
+                );
+                $splitVectors = $originalVector->div(5);
+
+                $combinedPoints = [
+                    $points[0],
+                ];
+
+                for ($n = 1; $n <= 10; $n++) {
+                    $combinedPoints[] = [
+                        $combinedPoints[$n-1][0] + $splitVectors->getValue()[0],
+                        $combinedPoints[$n-1][1] + $splitVectors->getValue()[1],
+                    ];
+                }
+
+                $path = vsprintf('M %d %d ', $points[0]);
+                $variatedLine = array_shift($points);
+
+                foreach (array_slice($combinedPoints, 1) as $newPoint) {
+                    $path .= vsprintf('A 1,1 0 0 1 %d,%d ', $newPoint);
+                }
+
+                foreach (array_slice($points, 1) as $linePoints) {
+                    $path .= vsprintf('L %d %d ', $linePoints);
+                }
+
+                $polygon = new SVGPath($path);
+                break;
+            case 'engrailed':
+                $points = $this->generatePoints(
+                    [
+                        [ -6,  0],
+                        [  6,  0],
+                        [  6,  8],
+                        [ -6,  8],
+                    ]
+                );
+                $originalVector = new Vector(
+                    $points[1][0] - $points[0][0],
+                    $points[1][1] - $points[0][1]
+                );
+                $splitVectors = $originalVector->div(5);
+
+                $combinedPoints = [
+                    $points[0],
+                ];
+
+                for ($n = 1; $n <= 10; $n++) {
+                    $combinedPoints[] = [
+                        $combinedPoints[$n-1][0] + $splitVectors->getValue()[0],
+                        $combinedPoints[$n-1][1] + $splitVectors->getValue()[1],
+                    ];
+                }
+
+                $path = vsprintf('M %d %d ', $points[0]);
+                $variatedLine = array_shift($points);
+
+                foreach (array_slice($combinedPoints, 1) as $newPoint) {
+                    $path .= vsprintf('A 1,1 0 0 0 %d,%d ', $newPoint);
+                }
+
+                foreach (array_slice($points, 1) as $linePoints) {
+                    $path .= vsprintf('L %d %d ', $linePoints);
+                }
+
+                $polygon = new SVGPath($path);
+                break;
+            case 'embattled':
+                $points = $this->generatePoints(
+                    [
+                        [ -6,  0],
+                        [  6,  0],
+                        [  6,  8],
+                        [ -6,  8],
+                    ]
+                );
+                $originalVector = new Vector(
+                    $points[1][0] - $points[0][0],
+                    $points[1][1] - $points[0][1]
+                );
+                $splitVectors = $originalVector->div(4);
+
+                $combinedPoints = [
+                    $points[0],
+                ];
+
+                for ($n = 1; $n <= 4; $n++) {
+                    $combinedPoints[] = [
+                        $combinedPoints[$n-1][0] + $splitVectors->getValue()[0],
+                        $combinedPoints[$n-1][1] + $splitVectors->getValue()[1],
+                    ];
+                }
+
+                $path = vsprintf('M %d %d ', $points[0]);
+                $variatedLine = array_shift($points);
+
+
+                /**
+                 * Embattled
+                 */
+
+                $pLeft = $this->generateVectorOffsetPoints(
+                    new Point(0, 0),
+                    new Point(1, 0),
+                    0,
+                    1.5 * $this->unitSize
+                );
+                $pForwards = $this->generateVectorOffsetPoints(
+                    new Point(0, 0),
+                    new Point(1, 0),
+                    1.5 * $this->unitSize,
+                    0
+                );
+                $pRight = $this->generateVectorOffsetPoints(
+                    new Point(0, 0),
+                    new Point(1, 0),
+                    0,
+                    -1.5 * $this->unitSize
+                );
+
+                for ($n = 1; $n <= 4; $n++) {
+                    $path .= vsprintf(
+                        ' l %d %d l %d %d l %d %d l %d %d',
+                        [
+                            $pForwards->x, $pForwards->y,
+                            $pLeft->x, $pLeft->y,
+                            $pForwards->x, $pForwards->y,
+                            $pRight->x, $pRight->y,
+                        ]
+                    );
+                }
+
+
+
+                foreach (array_slice($points, 1) as $linePoints) {
+                    $path .= vsprintf('L %d %d ', $linePoints);
+                }
+
+                $polygon = new SVGPath($path);
+                break;
+            default:
+                $points = $this->generatePoints(
+                    [
+                        [ -6,  0],
+                        [  6,  0],
+                        [  6, -6],
+                        [ -6, -6],
+                    ]
+                );
+                //$variatedLine = array_shift($points);
+
+                $path = vsprintf('M %d %d ', $points[0]);
+                foreach (array_slice($points, 1) as $linePoints) {
+                    $path .= vsprintf('L %d %d ', $linePoints);
+                }
+
+                $polygon = new SVGPath($path);
+                break;
+        }
+
+
+//        $polygon = new SVGPolygon(
+//            $this->generatePoints(
+//                [
+//                    [ -6, -6],
+//                    [  6, -6],
+//                    [  6,  0],
+//                    [ -6,  0],
+//                ]
+//            )
+//        );
 
         return $polygon;
     }
@@ -200,5 +555,43 @@ class HeraldicPartyGenerator
         }
 
         return $renderedPoints;
+    }
+
+    protected function generateVectorOffsetPoints(Point $start, Point $end, $vectorDistance, $offsetDistance) {
+        $vectorAngle = rad2deg(
+            atan2(
+                $end->y - $start->y,
+                $end->x - $start->x
+            )
+        );
+
+        $offsetAngle = deg2rad($vectorAngle + 90);
+
+        $newPoint = new Point(
+            $start->x + ($offsetDistance * (cos($offsetAngle))) + ($vectorDistance * (cos(deg2rad($vectorAngle)))),
+            $start->y + ($offsetDistance * (sin($offsetAngle))) + ($vectorDistance * (sin(deg2rad($vectorAngle))))
+        );
+        //var_dump($newPoint);
+
+        return $newPoint;
+    }
+
+
+}
+
+class Point
+{
+    public $x;
+    public $y;
+
+    /**
+     * Point constructor.
+     * @param $x
+     * @param $y
+     */
+    public function __construct($x, $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
     }
 }
